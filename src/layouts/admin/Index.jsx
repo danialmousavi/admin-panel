c29bf2f106936abee8335fae860d5dc82e6be68a
danialmousavi <danialmoosavi69@gmail.com>
2025-07-07 18:02:48 +0330

    create category page and componets and create react portal for modals

--- src/layouts/admin/Index.jsx
+++ src/layouts/admin/Index.jsx
@@ -2,19 +2,17 @@ import React, { useContext } from 'react'
 import Navbar from './navbar/Navbar'
 import Sidebar from './sidebar/Sidebar'
 import AdminContext, { AdminLayoutContainer } from '../../context/adminLayoutContext'
-import Dashboard from '../../pages/Dashboard/Dashboard'
+import Content from '../../pages/Content'
+
 
 export default function Index() {
-      const admincontext =useContext(AdminContext)
 
   return (
     <>
      <AdminLayoutContainer>
        <Navbar/>
       <Sidebar/>
-      <section id="content_section" className={`bg-light py-2 px-3 ${admincontext.showSidebar?"with_sidebar":""}`}>
-        <Dashboard/>
-      </section>
+        <Content/>
      </AdminLayoutContainer>
     </>
   )
---
+++ src/pages/Content.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import AdminContext from "../context/adminLayoutContext";
+import Category from "./Category/Category";
+
+export default function Content() {
+  const admincontext = useContext(AdminContext);
+
+  return (
+    <section
+      id="content_section"
+      className={`bg-light py-2 px-3 ${
+        admincontext.showSidebar ? "with_sidebar" : ""
+      }`}
+    >
+      {/* <Dashboard/> */}
+      <Category />
+    </section>
+  );
+}
