cab219346c7eca95e5d476caae8f9ff50fad1c15
danialmousavi <danialmoosavi69@gmail.com>
2025-07-22 13:31:35 +0330

    get users and show them in table

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -7,21 +7,8 @@ export default function Users() {
     <>
             <div id="manage_user_section" className="manage_user_section main_section ">
             <h4 className="text-center my-3">مدیریت کاربران</h4>
-            <div className="row justify-content-between">
-                <div className="col-10 col-md-6 col-lg-4">
-                    <div className="input-group mb-3 ltr-direction" >
-                        <input type="text" className="form-control" placeholder="قسمتی از نام یا نام خانوادگی را وارد کنید"/>
-                        <span className="input-group-text" >جستجو</span>
-                    </div>
-                </div>
-                <div className="col-2 col-md-6 col-lg-4 d-flex flex-column align-items-end">
-                    <button className="btn btn-success d-flex justify-content-center align-items-center" data-bs-toggle="modal" data-bs-target="#add_user_modal">
-                        <i className="fas fa-plus text-light"></i>
-                    </button>
-                </div>
-            </div>
+
             <UsersTable/>
-            <AddUser/>
         </div>
     </>
   )
--- src/pages/Users/UsersTable.jsx
+++ src/pages/Users/UsersTable.jsx
@@ -1,52 +1,83 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Actions from './tableAdditional/Actions';
+import PaginatedDataTable from '../../components/PaginatedDataTable';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 export default function UsersTable() {
+    const [data, setData] = useState([]);
+      const [loading, setLoading] = useState(false);
+      const [searchChar, setSearchChar] = useState("") 
+      const [currentPage, setCurrentPage] = useState(1) // صفحه حال حاضر
+      const [countOnPage, setCountOnPage] = useState(8) // تعداد محصول در هر صفحه
+      const [pageCount, setPageCount] = useState(0) // تعداد کل صفحات
+      const apiPath="https://ecomadminapi.azhadev.ir"
+
+        const dataInfo = [
+    { field: "id", title: "#" },
+
+    { field: "user_name", title: "نام کاربری" }, 
+    { field: "first_name", title: "نام" },
+    { field: "last_name", title: "نام خانوادگی" },
+    { field: "phone", title: "شماره تماس" },
+    { field: "email", title: "ایمیل" },
+    {
+      field: null,
+      title: "جنسیت",
+      elements: (rowData) => rowData.gender==1?"آقا":"خانم",
+    },
+    {
+      field: null,
+      title: "عملیات",
+      elements: (rowData) => <Actions rowData={rowData}  />,
+    },
+  ];
+  const searchParams = {
+    title: "جستجو",
+    placeholder: "قسمتی از عنوان را وارد کنید",
+  };
+    const handleGetProducts = async (page, count, char)=>{
+    const userToken=JSON.parse(localStorage.getItem("loginToken"))
+    setLoading(true)
+    await axios.get(`https://ecomadminapi.azhadev.ir/api/admin/users?page=${page}&count=${count}&searchChar=${char}`,{
+        headers:{
+            "Authorization":`Bearer ${userToken}`
+        }
+    }).then(res=>{
+        console.log(res);
+        res && setLoading(false)
+        if(res.status==200){
+        setData(res.data.data.data)//ست کردن دیتا
+        setPageCount(res.data.last_page)//ست کردن تعداد صفحات برای پجینیشن
+        }
+        
+    })
+  }
+
+  const handleSearch = (char)=>{
+    setSearchChar(char)
+    handleGetProducts(1, countOnPage, char)
+  }
+    useEffect(()=>{
+      handleGetProducts(currentPage, countOnPage, searchChar)
+    },[currentPage])
+  
   return (
     <>
-        <table className="table table-responsive text-center table-hover table-bordered">
-                <thead className="table-secondary">
-                    <tr>
-                        <th>#</th>
-                        <th>نام و نام خانوادگی</th>
-                        <th>موبایل</th>
-                        <th>ایمیل</th>
-                        <th>نقش </th>
-                        <th>تاریخ ثبت نام</th>
-                        <th>عملیات</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>قاسم بساکی</td>
-                        <td>09110110011</td>
-                        <td>mahdicmptr@gmail.com</td>
-                        <td>کاربر</td>
-                        <td>1400/10/12</td>
-                        <td>
-                            <i className="fas fa-edit text-warning mx-1 hoverable_text pointer has_tooltip" title="جزئیات و ویرایش کاربر" data-bs-toggle="modal" data-bs-placement="top" data-bs-target="#add_user_modal"></i>
-                            <i className="fas fa-times text-danger mx-1 hoverable_text pointer has_tooltip" title="حذف کاربر" data-bs-toggle="tooltip" data-bs-placement="top"></i>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <nav aria-label="Page navigation example" className="d-flex justify-content-center">
-                <ul className="pagination dir_ltr">
-                    <li className="page-item">
-                    <a className="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&raquo;</span>
-                    </a>
-                    </li>
-                    <li className="page-item"><a className="page-link" href="#">1</a></li>
-                    <li className="page-item"><a className="page-link" href="#">2</a></li>
-                    <li className="page-item"><a className="page-link" href="#">3</a></li>
-                    <li className="page-item">
-                    <a className="page-link" href="#" aria-label="Next">
-                        <span aria-hidden="true">&laquo;</span>
-                    </a>
-                    </li>
-                </ul>
-                </nav>
+     <PaginatedDataTable
+    tableData={data}
+    dataInfo={dataInfo}
+    searchParams={searchParams}
+    loading={loading}
+    currentPage={currentPage}
+    setCurrentPage={setCurrentPage}
+    pageCount={pageCount}
+    handleSearch={handleSearch}
+    >
+    <Link to={`/users/add-user`} className="btn btn-success d-flex justify-content-center align-items-center">
+        <i className="fas fa-plus text-light"></i>
+    </Link>
+    </PaginatedDataTable>
     </>
     )
 }
